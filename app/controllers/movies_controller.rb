fce4154566532420edc0aa3b8e1884026a4e214a
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-05-08 01:18:35 -0500

    configuring Movie routes in controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,31 @@
 class MoviesController < ApplicationController
 
-
-	get '/' do
-		#homepage/splashpage
-	erb :index
-	end
-
 	get '/movies' do
-		#list of all heroes movies <- post login
-	erb :'/movies'
-	end
+		redirect_if_not_logged_in
+		if current_hero.movies !=[]   #list of all heroes movies <- post login
+			@hero_movies = current_hero.movies.all 
+		end
+		@movies = Movie.all
+		erb :"/movies/index"
 
 	get '/movies/new' do
 		#on the heroes movie page <- post login
+		redirect_if_not_logged_in
+		@heroes = Hero.all
 		erb :'/movies/new'
 	end
+
+
+	post '/movies' do
+		movie = Movie.new(params)
+		if movie.save
+			redirect "/posts/#{movie.id}"
+		else
+			redirect 'movies/new'
+		end
+	end
+
+	
 
 	get '/movies/id' do
 		#on each instance of a movie
